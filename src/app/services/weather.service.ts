a7962dafa292eb971022418130519ea4fd10ddd7
peppemig <120139042+peppemig@users.noreply.github.com>
2023-01-19 22:24:07 +0100

    Update weather.service.ts
    removed api key

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -9,7 +9,7 @@ import { WeatherData } from '../model/weather.model';
 export class WeatherService {
 
   private requestLinkOne: string = 'https://api.openweathermap.org/data/2.5/weather?q=';
-  private requestLinkTwo: string = '&appid=211061300ac2acb85b19da2ea22ad805&units=metric';
+  private requestLinkTwo: string = '&appid=<api-token>&units=metric';
 
   constructor(private http: HttpClient) { }
 
